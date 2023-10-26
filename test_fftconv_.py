cfeb10c467ff26337020cf1fd918deeb8f3e97c8
caixin <caixin16@mails.ucas.ac.cn>
2023-10-26 11:27:40 +0800

    ccc

--- test_fftconv_.py
+++ test_fftconv_.py
@@ -3,8 +3,9 @@ import torch
 import numpy as np
 import cv2
 # from scipy.signal import fftconvolve
-A = [[1,1,1,1,1,0,0,0,0]]
-# A = [0,0,1,1,1,1,1,0,0]
+# A = [[1,1,1,1,1,0,0,0,0]]
+# A = [[0,0,1,1,1,1,1,0,0]]
+A = [[0,0,0,0,1,1,1,1,1]]
 # A = [0,0,1,1,1]
 
 B = [[1,1,1,1,1,1,1,1,1]]
@@ -13,4 +14,7 @@ B = torch.tensor(B).float()
 C = fftconvolve(A, B)
 
 # print(C[3:8])
-print(C)
+print("A = ",A)
+print("B = ",B)
+print("conv(A,B) = ", C)
+# print(C)
