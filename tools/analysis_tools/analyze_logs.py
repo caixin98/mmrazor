11aabd90bc54ef7e0def297c10a553687146bff0
caixin <caixin16@mails.ucas.ac.cn>
2023-11-17 23:27:54 +0800

    ccc

--- tools/analysis_tools/analyze_logs.py
+++ tools/analysis_tools/analyze_logs.py
@@ -73,7 +73,8 @@ def plot_phase_train(metric, log_dict, epochs, curve_label, json_log):
         ys.append(np.array(log_dict[epoch][metric]))
     xs = np.concatenate(xs)
     ys = np.concatenate(ys)
-    save_dict = {"ys":list(ys[:50])}
+    print(xs.shape, ys.shape)
+    save_dict = {"ys":list(ys[::50])}
     save_path = json_log.split('.json')[0] +"_" + "loss" + '.json'
     print(save_dict,save_path)
     with open(save_path, 'w') as f:
